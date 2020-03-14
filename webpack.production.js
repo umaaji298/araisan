6f42349f3037c639add979b2706ee569d8b10a33
umaaji298 <umaaji298@gmail.com>
2020-03-14 21:02:39 +0900

    production build

---
+++ webpack.production.js
@@ -0,0 +1,48 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+const webpack = require("webpack");
+
+const pathToPhaser = path.join(__dirname, "/node_modules/phaser/");
+const phaser = path.join(pathToPhaser, "dist/phaser.js");
+
+module.exports = {
+  context: __dirname,
+  entry:{
+    'game': './app/game.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './docs'),
+    filename: "game.js",
+    publicPath:"/docs/"
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, loader: "babel-loader", exclude: "/node_modules/" },
+      //phaser-hogehoge.jsというファイルの内容はPhaserというグローバル変数に内容を突っ込む(expose-loader)
+      { test: /phaser\.js$/, loader: "expose-loader?Phaser" }
+    ]
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true,
+  },
+  resolve: {
+    //バンドル対象にするファイルを指定する
+    extensions: [".js"],
+    //import "phaser"ってしたときに読み込みに行くやつを指定する
+    alias: {
+      phaser: phaser
+    }
+  },
+  mode: 'production',
+  optimization: {
+    minimize: true
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      CANVAS_RENDERER: JSON.stringify(true),
+      WEBGL_RENDERER: JSON.stringify(true)
+    })
+  ]
+};
