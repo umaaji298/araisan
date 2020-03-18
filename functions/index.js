0ccb720c53261a033d3b28b3cfe939e5956557ef
umaaji298 <umaaji298@gmail.com>
2020-03-18 21:05:14 +0900

    event all registered

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,11 @@ admin.initializeApp();
 
 const db = admin.firestore();
 
-exports.appendEventJson = functions
+/**
+ * DBの変更をトリガーに、events.jsonを生成する
+ */
+
+exports.createDailyEvents = functions
   .region('asia-northeast1')
   .firestore
   .document('floors/v1/datas/{dataid}')
@@ -17,46 +21,70 @@ exports.appendEventJson = functions
     // const data = change.after.data();
     // console.log('document change',id,data);
 
-    // DB Event全て読み出し、jsonを作成する
-    const writeData = new Object();
-    const collectionData = await db.collection('floors/v1/datas').get();
+    console.log('create event.json');
 
-    //console.log('length' ,collectionData.docs.length);
-
-    for(let i = 0;i<collectionData.docs.length;i++)
-    {
-      const doc = collectionData.docs[i];
-      const data = doc.data();
-
-      writeData[doc.ref.id] = {
-        text : data.text,
-        imgUrl : data.imgUrl,
-        floorName : data.floorName,
-        author : data.author
-      }
-    }
-
-    //default bucket
-    const bucket = admin.storage().bucket();
-    const file = bucket.file('events.json');
-
-    const writeStream = file.createWriteStream({
-      public: true, // ACL public
-      gzip: true,
-      metadata: {
-        cacheControl: "public, max-age=2592000, no-transform",
-        contentType: 'application/json'
-      }
-    });
-
-    // //書き込み本体
-    await writeHtmlCore(JSON.stringify(writeData),writeStream);
+    await createEventsJson();
 
     console.log('done');
 
     return
   });
 
+// exports.scheduledFunction = functions
+//   .region('asia-northeast1')
+//   .functions.pubsub.schedule('5 4 * * *') // This will be run every day at 4:05 AM
+//   .timeZone('Asia/Tokyo')
+//   .onRun(async (context) => {
+
+//     console.log('create event.json');
+
+//     await createEventsJson();
+
+//     //todo delete daily db table
+//     //delete events_daily.json
+
+//     console.log('done');
+
+//     return null;
+//   });
+
+async function createEventsJson() {
+  // DB Event全て読み出し、jsonを作成する
+  const writeData = new Object();
+  const collectionData = await db.collection('floors/v1/datas').get();
+
+  //console.log('length' ,collectionData.docs.length);
+
+  for (let i = 0; i < collectionData.docs.length; i++) {
+    const doc = collectionData.docs[i];
+    const data = doc.data();
+
+    writeData[doc.ref.id] = {
+      text: data.text,
+      imgUrl: data.imgUrl,
+      floorName: data.floorName,
+      author: data.author
+    }
+  }
+
+  //default bucket
+  const bucket = admin.storage().bucket();
+  const file = bucket.file('events.json');
+
+  const writeStream = file.createWriteStream({
+    public: true, // ACL public
+    gzip: true,
+    metadata: {
+      cacheControl: "public, max-age=300, no-transform",
+      contentType: 'application/json'
+    }
+  });
+
+  // //書き込み本体
+  await writeHtmlCore(JSON.stringify(writeData), writeStream);
+
+  return
+}
 
 function writeHtmlCore(data, writeStream) {
   return new Promise(resolve => {
