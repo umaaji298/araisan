d959fba3cd8a8914b1f4a777f04f9933abdeb14c
umaaji298 <umaaji298@gmail.com>
2020-03-25 23:25:39 +0900

    editor fix for new tag

--- app/scenes_editor/editor.js
+++ app/scenes_editor/editor.js
@@ -11,14 +11,22 @@ export default class Editor extends Phaser.Scene {
   preload() {
     //console.log('call game pleroad');
 
+    this.ev2data = [
+      ["無が", "みんみが", "オオカワウソが", "ガチおじが", "死が", "怪異が", "おじぞうさんが", "かたまりが", "人形が", "全裸の異性が", "としあきが", "地下ラッコが", "コツメカワウソが", "荒耶宗蓮が", "メリーさんが", "探索者が", "ギンギツネが", "アライさんが", "フェネックが", "きんたまが", "モブフレンズが", "肉食ちゃんが", "草食ちゃんが", "木魚マンが", "お稲荷様が", "目玉が", "ネズミボトルが", "待機カワウソが", "じごくボスが"],
+      ["壁が", "裸フレンズが", "ケルピーが", "ニセイさんが", "図書館が", "クラウケンが", "異形が", "古狩人が", "ジャンレノが", "宇宙人が", "ボンドルド卿が", "霊体が", "かーさばが", "壁の絵が", "キタキツネが", "警察が", "ホオジロザメが", "狐ババアが", "ショタが", "ピザ屋が", "狂犬フレンズが", "ワシさんが", "「あけて」君が", "死体が", "\\G人ミサキが", "光が", "闇が", "悪鬼が", "機械が"],
+      ["サーバールームが", "エレベーターガールが", "妖精みんみが", "エリクサーが", "イクラが", "トルソーが", "尿ボトルが", "尻目が", "ボスが", "家具が", "エガちゃんが", "ジャガーさんが", "オナガザメが", "メリヰさんが", "エンジニアが", "ソフトクリームが", "ハカセが", "助手が", "スナドリネコが", "メカオオカワウソが", "島が", "フロアが", "自宅が", "パソコンが", "マンション住人が", "全員が", "スマフォが", "ゾンビが", "旧神が"],
+    ];
+
     //gauge 数値データ置き換え
-    this.numTag = ["１／２", "７", "５", "７７", "３", "１０", "０．１", "１", "千万", "２", "１２", "０", "（検閲）", "無", "百万", "０．０１","５０００兆"];
+    this.numTag = ["１／２", "８", "１０", "７７", "３", "７", "０．１", "１", "１００", "２", "１２", "０", "（検閲）", "無", "百万", "０．０１", "５０００兆", "８"];
   }
 
   create() {
     console.log('%c editor ', 'background: green; color: white; display: block;');
 
     this.cameras.main.fadeIn(1000, 0, 0, 0);
+
+    this.randDatas = getRandData({}, this);
 
     //TCRP event
     this.myCmds = new TcrpAction(this);
@@ -67,15 +75,11 @@ export default class Editor extends Phaser.Scene {
 function readEventsText(scene) {
   //formより読み込み
   const event_raw = document.getElementById('eventText').value;
-  const event = event_raw.split('\n').join(',')
+  let script = event_raw.split('\n').join(',')
+  console.log('evet text', script);
+  script = tagReplace(script, scene);
 
-  console.log('evet text', event);
-
-  //\Gの置き換え
-  const index = Phaser.Math.Between(0, scene.numTag.length - 1);
-  const fixevent = event.replace(/\\G/g, scene.numTag[index]); // ランダム表示
-
-  return util.scriptsToEvents(fixevent);
+  return util.scriptsToEvents(script);
 }
 
 function tcrpPlay(scene, commands) {
@@ -88,4 +92,43 @@ function tcrpPlay(scene, commands) {
     .on('complete', function () {
       console.log(scene.player.now * 0.001);
     });
+}
+
+function getRandData(floorRand, scene) {
+  const retObj = new Object();
+
+  retObj.numtagId = Phaser.Math.Between(0, scene.numTag.length - 1);
+  retObj.tebleId = Phaser.Math.Between(0, scene.ev2data.length - 1);
+  retObj.npcId = Phaser.Math.Between(0, scene.ev2data[0].length - 1);
+
+  return retObj;
+}
+
+function tagReplace(text, scene) {
+  const replacerScene = replacer.bind(scene);
+  return text.replace(/\\[A-Z]/g, replacerScene);
+}
+
+function replacer(match) {
+  //console.log(match,this.randDatas.tebleId,this.randDatas.npcId);
+
+  let replacedStr = "";
+  switch (match) {
+    case "\\G": {
+      //floorRand is var
+      replacedStr = this.numTag[this.randDatas.numtagId];
+      break;
+    }
+    case "\\C": {
+
+      replacedStr = this.ev2data[this.randDatas.tebleId][this.randDatas.npcId].slice(0, -1);
+      break;
+    }
+    default: {
+      console.log('unknown tag', match);
+      //replacedata is blank
+      break;
+    }
+  }
+  return replacedStr;
 }
