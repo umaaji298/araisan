ef18cfa2f30a6dc1d4f9a6c857f958f4410e84f9
umaaji298 <umaaji298@gmail.com>
2020-04-19 13:48:47 +0900

    faq

---
+++ firestore/searchFloorData.js
@@ -0,0 +1,33 @@
+
+const admin = require("firebase-admin");
+
+const serviceAccount = require("../keys/owner.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://araisan-ms.firebaseio.com"
+});
+
+
+let db = admin.firestore();
+
+async function main(){
+
+  // const writeBatch = db.batch();
+
+  const ss = await db.collectionGroup(`datas`).get(); 
+
+  ss.forEach(doc=>{
+    const data = doc.data();
+    if(doc.ref.id === "18062725"){
+      console.log('found',doc.ref.id,doc.ref.parent.parent.id);
+      // writeBatch.delete(doc.ref);
+    }   
+  })
+
+  // await writeBatch.commit();
+}
+
+main().then(()=>{
+  console.log('done');
+});
